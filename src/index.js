607316be95766d877f0ea4415f0283d7e30bf80c
Danielle Rodrigues <livibellyrds@gameil.com>
2021-11-26 20:11:15 -0300

    frontend

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
+const path =require('path');
 const  express = require('express');
 const router = require("./router");
 
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'web')))
+
+app.get('/', function (_, res) {
+  res.render('index')
+})
 const db = require("./config/db");
 //exclui a tabela resincroniza a base de dados
 db.sequelize.sync({ force: true }).then(() => { 
--- src/web/script.js
+++ src/web/script.js
@@ -1,21 +1,97 @@
- function listaDeliveries() {
-  let tbody = document.getElementById("tbody");
+class Delivery {
 
-  for (let i = 0; i < array.arrayDeliveries.length; i++) {
-    let tr = tbody.insertRow();
-
-    let td_nome = tr.insertCell();
-    let td_peso = tr.insertCell();
-    let td_endereco = tr.insertCell();
-    let td_telefone = tr.insertCell();
-    let td_acoes = tr.insertCell();
-
-    td_nome.innerText = this.arrayDeliveries[i].nome;
-    td_peso.innerText = this.arrayDeliveries[i].peso;
-    td_endereco.innerText = this.arrayDeliveries[i].endereco;
-    td_telefone.innerText = this.arrayDeliveries[i].telefone;
-    td_acoes.innerText = "excluir";
-
-    td.acoes.classList.add("center");
+  constructor() {
+    this.arrayEntrega = [];
   }
+
+  lerDados() {
+    let entrega = {};
+
+    entrega.nome = document.getElementById("nome").value;
+    entrega.peso = document.getElementById("peso").value;
+    entrega.endereco = document.getElementById("endereco").value;
+    entrega.telefone = document.getElementById("telefone").value;
+
+    return entrega;
+  }
+
+  listar() {
+    let tbody = document.getElementById("tbody");
+
+    tbody.innerHTML = "";
+
+    for (let i = 0; i < this.arrayEntrega.length; i++) {
+      let tr = tbody.insertRow();
+      
+      let td_nome = tr.insertCell();
+      let td_peso = tr.insertCell();
+      let td_endereco = tr.insertCell();
+      let td_telefone = tr.insertCell();
+      let td_acoes = tr.insertCell();
+
+
+      td_nome.innerText =  this.arrayEntrega[i].nome;
+      td_endereco.innerText = this.arrayEntrega[i].endereco;
+      td_telefone.innerText = this.arrayEntrega[i].telefone;
+      td_peso.innerText =  this.arrayEntrega[i].peso;
+
+      td_acoes.innerHTML =  `<span class='btn' onclick='entrega.excluir(${this.arrayEntrega[i].id})'>excluir</span>`;
+      td_acoes.classList.add("center");
+
+    }
+  }
+
+  cadastrar() {
+    let entrega = this.lerDados();
+    this.adicionar(entrega);
+  }
+
+  async adicionar(entrega) {
+    const result = await fetch("/deliveries", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(entrega)
+    });
+    const content = await result.json();
+    this.arrayEntrega.push(entrega);
+    this.buscar();
+  }
+  
+  limpar() {
+    document.getElementById("nome").value = "";
+    document.getElementById("peso").value = "";
+    document.getElementById("endereco").value = "";
+    document.getElementById("telefone").value = "";
+  }
+
+  async excluir(id) {
+    const result = await fetch(`/deliveries/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    });
+    const content = await result.json();
+    alert(content[0]);
+    this.buscar();
+  }
+
+  async buscar() {
+    this.arrayEntrega = [];
+
+    let result = await fetch("/deliveries");
+    let json = await result.json();
+    json.forEach(delivery => {
+      this.arrayEntrega.push(delivery);
+    });
+    entrega.listar();
+  }
+
 }
+
+var entrega = new Delivery();
+entrega.buscar();
